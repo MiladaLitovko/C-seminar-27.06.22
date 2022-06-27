37ad326252f9922714eb7e5cd58bb73cbf7af54d
litovkomilada <litovkomiada@yandex.ru>
2022-06-27 21:02:59 +0300

    Complete 001

---
+++ 001/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rand = new Random();
+int number = rand.Next(10, 100);
+Zadacha9(number);
+
+
+void Zadacha9(int currentNumber)
+{
+    Console.WriteLine(currentNumber);
+    int digitFirst = currentNumber/10;
+    int digitSecond = currentNumber%10;
+    
+    if(digitFirst > digitSecond)
+    {
+        Console.WriteLine(digitFirst);
+        }
+    else if(digitFirst < digitSecond)
+    {
+        Console.WriteLine(digitSecond);
+        }
+    else
+    {
+        Console.WriteLine("цифры числа равны");
+    }
+}
