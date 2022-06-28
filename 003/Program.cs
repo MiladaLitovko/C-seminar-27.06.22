e7bf279a016981311b0ed2219acfbb51ed1e4f37
litovkomilada <litovkomiada@yandex.ru>
2022-06-28 16:37:12 +0300

    Complete 003

---
+++ 003/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+12. Напишите программу, которая будет принимать
+на вход два числа и выводить, является ли второе
+число кратным первому. Если число 2 не кратно числу
+1, то программа выводит остаток от деления.
+*/
+
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a % b == 0)
+{
+    Console.WriteLine("кратные числа");
+}
+else
+{
+    Console.WriteLine(a % b);
+}
