3a08cc09c3af758847fbd75c41fe6c390c1e529e
litovkomilada <litovkomiada@yandex.ru>
2022-06-28 16:14:27 +0300

    Complete 001

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,4 +1,8 @@
-﻿Random rand = new Random();
+﻿/*9. Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа.*/
+
+Random rand = new Random();
 int number = rand.Next(10, 100);
 Zadacha9(number);
 
