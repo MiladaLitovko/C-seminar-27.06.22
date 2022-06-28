40939e6fb4c82cdd092354249674c6a15710b742
litovkomilada <litovkomiada@yandex.ru>
2022-06-28 16:26:08 +0300

    Complete 002

---
+++ 002/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+Random rand = new Random();
+//int number = rand.Next(100, 1000);
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();
+//Console.WriteLine(number);
+Zadacha11(number);
+
+
+void Zadacha11(string number)
+{
+    Console.Write(number[0]);
+    Console.Write(number[2]);
+    Console.WriteLine();
+}
